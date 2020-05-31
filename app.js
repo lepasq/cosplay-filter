a38f54eb683c76737e0a6b4936ac4f1b504532d1
lepasq <pasquarelli.le@gmail.com>
2020-05-31 22:14:48 +0200

    Add table structure and search bar

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const apiRouter = require("./src/api/apiRouter");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.static('public'));
-app.use("/", apiRouter);
 
+app.use("/", apiRouter);
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
