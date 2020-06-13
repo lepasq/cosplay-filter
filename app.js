3e07530008106dd9fe144f76244b16d0a007b3e0
lepasq <pasquarelli.le@gmail.com>
2020-06-13 14:48:09 +0200

    Setup MySQL database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const apiRouter = require("./src/api/apiRouter");
 const path = require("path");
+const bodyParser = require("body-parser");
+require('dotenv').config();
 
 const app = express();
+const mysql = require('./src/models/connection.js');
 const PORT = process.env.PORT || 4000;
 
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended:true
+    })
+);
 
 app.use("/", apiRouter);
 app.use('/', express.static(__dirname + '/public'));
---
+++ src/models/connection.js
@@ -0,0 +1,19 @@
+let mysql = require('mysql');
+
+let pool = mysql.createPool({
+    connectionLimit: process.env.CONNECTION_LIMIT,
+    host: process.env.HOST,
+    user: process.env.USR,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+
+ pool.query('SELECT c.*, c.name, GROUP_CONCAT(t.tag) as tags  FROM Cosplay.Character c, Cosplay.Tag t WHERE t.cid = c.id GROUP BY c.id;', (err, rows, fields) => {
+     if(err) {
+         throw err;
+     }
+     console.log('Connected Database.')
+     console.log(rows[0]);
+ });
